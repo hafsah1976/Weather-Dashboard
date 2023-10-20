aa3d1d26fa6ab3f51de6bc1d9128a67cf7814f21
Khan-HB <hafsah999@gmail.com>
2023-10-20 03:06:23 -0400

    added the five-day forecast cards in html file in my last commit. working on js function to get searchHistory from localStorage

---
+++ script.js
@@ -0,0 +1,10 @@
+//assign apikey to a variable 
+var apiKey = '';
+
+// Initialize an empty array to store saved searches
+var savedSearches = [];
+
+// Function to display search history
+var searchHistory = function(yourInput) {
+    
+}
